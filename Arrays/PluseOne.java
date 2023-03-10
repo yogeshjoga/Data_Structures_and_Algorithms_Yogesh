d0d2ba547504e25b76c93a8155d9780bea3bedb0
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-11 00:03:17 +0530

    pluse one main solution
    repate everything every weekend orther wise you will loose the coding skills with  in month

--- Arrays/PluseOne.java
+++ Arrays/PluseOne.java
@@ -11,10 +11,21 @@ public class PluseOne {
          * little crazy about this question
          * 
          */
+        int[] arr= new int[nums.length+1];
+
         if(nums[nums.length-1] == 9){
+            
+            arr = nums;
+            arr[nums.length-1] = 1;
+
+            nums[nums.length-1] = 1;
+            nums[nums.length-1] = 0;
+        }else{
             nums[nums.length-1] =  nums[nums.length-1]+1;
         }
-        nums[nums.length-1] =  nums[nums.length-1]+1;
+     //   nums[nums.length-1] =  nums[nums.length-1]+1;
+
+
         // int plus;
         // for(int i=0; i<nums.length-1; i++){
         //     //1234
@@ -29,15 +40,38 @@ public class PluseOne {
       for (int i : nums) {
         System.out.println(i);
       }
+
+      for (int i : arr) {
+        System.out.println(i);
+        
+      }
         
 
         return nums;
+    }
+
+    public int[] solution(int[] nums){
+        
+            int n = nums.length;
+             for(int i=n-1; i>=0; i--) {
+                 if(nums[i] < 9) {
+                    nums[i]++;
+                     return nums;
+                 }
+                 nums[i] = 0;
+             }
+             int[] newNumber = new int [n+1];
+             newNumber[0] = 1;
+             return newNumber;
+         
+         
     }
 
     public static void main(String[] args) {
         PluseOne pluseOne = new PluseOne();
         int[] nums = {1,2,3,4,5,9};
         pluseOne.testSolution(nums);
+        pluseOne.solution(nums);
     }
     
 }

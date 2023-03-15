fa18f9a0e6df4f4798a8547a0d39db5aa3cfb203
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-15 23:57:12 +0530

    day one

---
+++ Arrays/ConvertSortedArrayToBinarySearch.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+
+public class ConvertSortedArrayToBinarySearch {
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+  
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        
+        return sortedArrayToBST(nums, 0, nums.length-1);
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums, int i, int j) {
+        if (i > j) return null;
+        int mid = (i + j) / 2;
+        TreeNode res = new TreeNode(nums[mid]);
+        if (i == j) return res;
+        res.left = sortedArrayToBST(nums, i, mid-1);
+        res.right = sortedArrayToBST(nums, mid+1, j);
+        return res;
+    }
+    
+
+    public static void main(String[] args) {
+        /*
+         * here we need to learn binarySearch
+         */
+    }
+}
---
+++ Programmming_skills/DayOne.java
@@ -0,0 +1,54 @@
+package Programmming_skills;
+
+import java.util.Arrays;
+
+public class DayOne {
+    // this is accepted but Time Limite exceed! Not valid solution
+    public int countOdds(int low, int high) {
+        int count = 0;
+        for(int i=low; i<=high; i++){
+            if(i%2!=0){
+            count++;
+            }
+        } 
+        return count;
+    }
+ 
+    public int countOdds1(int low, int high) {
+        if(low % 2 == 0 && high % 2 == 0){
+            return (high-low) /2;
+        }
+         return (high-low) / 2+1;
+     }
+
+     // day one 2nd question
+     public double average(int[] salary) {
+        Arrays.sort(salary);
+        double total = 0;
+        int len = salary.length;
+        for(int i=0; i<len; i++){
+            total = total + salary[i];
+        }
+        System.out.println(total/len);
+       return (total/(salary.length-2));
+    }
+
+    public double average2(int[] salary) {
+        final int N = salary.length;
+        Arrays.sort(salary);
+       double t = salary[1];
+        for (int i = 2; i < N - 1; i++) {
+            t += salary[i];
+        }
+        System.out.println(t / (N - 2));
+        return t / (N - 2);
+    }
+
+
+    public static void main(String[] args) {
+        int[] salary ={8000,9000,2000,3000,6000,1000};
+        DayOne dayOne = new DayOne();
+      dayOne.average(salary);
+      //  dayOne.average2(salary);
+    }
+}

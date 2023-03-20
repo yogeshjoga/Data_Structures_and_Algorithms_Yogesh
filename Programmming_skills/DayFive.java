050af015523705b65f228cd9be18333e2ae0b61a
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-20 14:26:10 +0530

    DayFive completed lets once revise the whole concepts
    understand the concepts
    like functions and methods
    
    mostly see the recursive methods and
    for loops and conditions and more

---
+++ Programmming_skills/DayFive.java
@@ -0,0 +1,15 @@
+package Programmming_skills;
+
+public class DayFive {
+    /*
+     * Day five: functions/Methods
+     * lets develop the methods to use and slove the complex problems
+     * ---Methods
+     */
+    
+
+
+     public static void main(String[] args) {
+        DayFive dayFive = new DayFive();
+     }
+}

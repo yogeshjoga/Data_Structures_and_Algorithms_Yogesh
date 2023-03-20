623561c22a42576dbdd48e104db4a0b09e87e169
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-20 23:54:46 +0530

    Day eight adv LinkedList
    learn recursion
    most of solution recursion
    based on sloutions

---
+++ DSA_1_Preparation_LeetCode_I/DayEight.java
@@ -0,0 +1,68 @@
+package DSA_1_Preparation_LeetCode_I;
+
+public class DayEight {
+/*
+ * Day eight
+ * DSA part 1
+ * LinkedList advance problems to solve 
+ * lets understand the questions and solutions
+ * im just copy pasting the solutions from internet
+ * to understand the flow and resolve mySelf
+ */
+
+ //206. Reverse Linked List
+ // 1st solution advance LinkedList
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+//}
+
+
+
+//83. Remove Duplicates from Sorted List
+// 2nd solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+//class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+//}
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}

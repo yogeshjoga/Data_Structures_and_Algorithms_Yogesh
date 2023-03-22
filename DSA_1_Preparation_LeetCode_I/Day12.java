0e2bca3cd9e0fe841a63d27c9f1cd54b8ea0ecf3
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-22 21:41:53 +0530

    Day 12
    tree
     depth first search algo

---
+++ DSA_1_Preparation_LeetCode_I/Day12.java
@@ -0,0 +1,25 @@
+package DSA_1_Preparation_LeetCode_I;
+
+
+
+public class Day12 {
+
+/*
+ * Advance tree problems
+ * Depth first search Algo
+ */
+
+ //226. Invert Binary Tree --> its mean mirror the binary tree
+ // 1st solu
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}

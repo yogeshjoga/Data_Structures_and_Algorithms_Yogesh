22568c5603341459641786bec8bf4b16d1c02f68
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2023-03-26 23:56:34 +0530

    Update Day12.java

--- Programmming_skills_LeetCode/Day12.java
+++ Programmming_skills_LeetCode/Day12.java
@@ -29,7 +29,31 @@ public class Day12 {
 
 
 //303. Range Sum Query - Immutable
-// 2nd soluiton
+// 2nd solution
+class NumArray {
+
+    public NumArray(int[] nums) {
+     prefix = new int[nums.length + 1];
+     for (int i = 0; i < nums.length; ++i)
+       prefix[i + 1] = nums[i] + prefix[i];
+   }
+ 
+   public int sumRange(int left, int right) {
+     return prefix[right + 1] - prefix[left];
+   }
+ 
+   private int[] prefix;
+ }
+ 
+ /**
+  * Your NumArray object will be instantiated and called as such:
+  * NumArray obj = new NumArray(nums);
+  * int param_1 = obj.sumRange(left,right);
+  */
+
+
+hello this
+  
 
     public static void main(String[] args) {
         
